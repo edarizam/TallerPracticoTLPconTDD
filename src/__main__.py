f66aaa2e257fcf545c73e328d8391dd58375d183
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-09-23 01:00:47 -0500

    commit

---
+++ src/__main__.py
@@ -0,0 +1,30 @@
+"""
+Integrantes: 
+• Juan José Gonzalez Morales
+• Edinson Andres Ariza Mendoza 
+
+Aplicación: 
+
+La aplicacion implementada es sobre un banco virtual como por ejemplo nequi, en donde el usuario podra acceder
+a las siguientes funcionalidades:
+
+• Inicio de sesion y creación de una nueva cuenta: En esta funcionalidad el usuario podra iniciar sesion en su
+cuenta existente o crear una nueva cuenta a su nombre con su numero de telefono y una contraseña de 4 digitos numericos. 
+(Para esto se usan las clases CuentaVirtual y Usuario ubicadas en sus respectivos módulos)
+
+• Consignación, retiro y envio de dinero: Cada usuario creado tendra una cuenta virtual asociada a la cual podrá
+consignar, retirar y enviar dinero a otro a usuario unicamente a otro usuario que este registrado en el banco 
+virtual de la aplicacion. (Para esto se usan las clases BancoVirtual, CuentaVirtual, Usuario ubicadas en sus 
+respectivos módulos).
+
+• Pago de servicios: 
+
+
+
+
+Requisitos del sistema:
+
+Para el correcto funcionamiento de la aplicacion es necesaria la previa creacion de una instancia de BancoVirtual
+y las respectivas instancias de los servicios que se necesitan.
+
+"""
