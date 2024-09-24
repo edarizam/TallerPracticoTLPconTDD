8dc74d3583938b2fe56e82513f060c502668d68c
juangonzalezmo <juagonzalezmo@unal.edu.co>
2024-09-23 21:55:56 -0500

    mejor

--- src/__main__.py
+++ src/__main__.py
@@ -1,33 +1,3 @@
-"""
-Integrantes: 
-• Juan José Gonzalez Morales, C.C. 1013458547
-• Edinson Andres Ariza Mendoza, C.C. 1099737114
-
-Aplicación: 
-
-La aplicacion implementada es sobre un banco virtual como por ejemplo nequi, en donde el usuario podra acceder
-a las siguientes funcionalidades:
-
-• Inicio de sesion y creación de una nueva cuenta: En esta funcionalidad el usuario podra iniciar sesion en su
-cuenta existente o crear una nueva cuenta a su nombre con su numero de telefono y una contraseña de 4 digitos numericos. 
-(Para esto se usan las clases CuentaVirtual y Usuario ubicadas en sus respectivos módulos)
-
-• Consignación, retiro y envio de dinero: Cada usuario creado tendra una cuenta virtual asociada a la cual podrá
-consignar, retirar y enviar dinero a otro a usuario unicamente a otro usuario que este registrado en el banco 
-virtual de la aplicacion. (Para esto se usan las clases BancoVirtual, CuentaVirtual, Usuario ubicadas en sus 
-respectivos módulos).
-
-• Pago de servicios: En esta funcionalidad, según los servicios con los que tengamos algún convenio, el usuario
-podrá realizar el pago de planes mensuales asociados al servicio
-
-Requisitos del sistema:
-
-Para el correcto funcionamiento de la aplicacion es necesaria la previa creacion de una instancia de BancoVirtual
-y las respectivas instancias de los servicios que se necesitan.
-
-Nota: Se recomienda ejecutar la palicacion con la consola grande.
-
-"""
 
 #Solucionamos problemas con las importaciones
 import sys, os
